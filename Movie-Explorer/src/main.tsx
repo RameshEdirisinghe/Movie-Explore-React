279e8290c05e63f3cac4538721605f34544b3e27
Ramesh Edirisinghe <e2145434@bit.uom.lk>
2025-05-10 12:47:30 +0530

    config tailwind css

--- Movie-Explorer/src/main.tsx
+++ Movie-Explorer/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Change document title
+document.title = 'MovieExplorer - Discover Your Favorite Films';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
